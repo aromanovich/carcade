d71db5c75d4e241cf56b0c7e120ca5add5cb060a
Anton Romanovich <anthony.romanovich@gmail.com>
2013-01-08 16:47:50 +0600

    Change build process to get access to the sibling pages in templates

--- carcade/core.py
+++ carcade/core.py
@@ -12,9 +12,6 @@ sys.path.append(os.getcwd())
 import carcade_settings
 
 
-PAGES_PREFIX = 'pages/'
-
-
 def path_for(name, language=None):
     url = ''
     if language != carcade_settings.DEFAULT_LANGUAGE:
@@ -83,36 +80,79 @@ def create_context(page_dir, language):
     return context
 
 
+class Page(object):
+    def __init__(self, name, target_filename, context):
+        self.name = name
+        self.context = context
+        self.target_filename = target_filename
+        self.children = []
+        self.ascendant = None
+
+    def add_child(self, page):
+        self.children.append(page)
+        page.ascendant = self
+
+
 def build(build_dir):
+    PAGES_DIR = 'pages/'
+
     for language in carcade_settings.LANGUAGES:
-        contexts = {}
         jinja2_env = create_jinja2_environment(language)
-
-        for root, directories, files in os.walk(PAGES_PREFIX, topdown=False):
-            page_name = root[len(PAGES_PREFIX):]
-
-            if not page_name:
-                # Reached the top. We're done
-                break
+        
+        # Build site tree from down to top
+        forest = {}  # `forest` contains growing site subtrees
+        for root, directories, files in os.walk(PAGES_DIR, topdown=False):
+            page_name = root[len(PAGES_DIR):]
 
             context = create_context(root, language)
             context['PAGE_NAME'] = page_name
 
-            # Store context — we will need it to construct ascendant page context
-            contexts[page_name] = context
-
-            # Retrieve template
-            template = jinja2_env.get_template(
-                carcade_settings.LAYOUTS.get(page_name, carcade_settings.DEFAULT_LAYOUT))
-
-            subpages = []
-            for directory in directories:
-                # Collect descendant page contexts into `subpages` list
-                subpage_name = os.path.join(page_name, directory)
-                subpages.append(contexts[subpage_name])
-
             target_filename = os.path.join(
                 build_dir, path_for(page_name, language=language), 'index.html')
-            # Important: don't change stored context by adding `SUBPAGES`
-            context = dict(context, SUBPAGES=subpages)
-            render_template(template, context, target_filename)
+
+            page = Page(page_name, target_filename, context)
+            for directory in directories:
+                child_name = os.path.join(page_name, directory)
+                child = forest.pop(child_name)  # Pop subtree from `forest`
+                page.add_child(child)  # Attach it to ascendant page
+
+            forest[page.name] = page  # Put resulting subtree to the `forest`
+
+        root = forest.pop('')
+        assert not forest
+
+        build_recursive(jinja2_env, root)
+
+
+def build_recursive(jinja2_env, page):
+    for child in page.children:
+        build_recursive(jinja2_env, child)
+
+    if not page.name:
+        # `page` is root, nothing to build
+        return
+
+    # Retrieve template
+    template = jinja2_env.get_template(
+        carcade_settings.LAYOUTS.get(page.name, carcade_settings.DEFAULT_LAYOUT))
+
+    # Collect descendant page contexts
+    subpages = []
+    for subpage in page.children:
+        subpages.append(subpage.context)
+
+    # Collect sibling page contexts
+    index = None
+    siblings = []
+    for idx, sibling in enumerate(page.ascendant.children):
+        if sibling == page:  # TODO Introduce pagination
+            index = idx
+        siblings.append(sibling.context)
+
+    # Important: don't change stored context by adding new keys
+    context = dict(page.context, **{
+        'SUBPAGES': subpages,  # TODO Make naming more consistent
+        'SIBLINGS': siblings,
+        'INDEX': index  # XXX
+    })
+    render_template(template, context, page.target_filename)

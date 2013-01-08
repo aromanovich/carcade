549f77cff3976c3baa14abf7c315ac7766422088
Anton Romanovich <anthony.romanovich@gmail.com>
2013-01-09 00:56:27 +0600

    Refactoring

--- carcade/core.py
+++ carcade/core.py
@@ -12,6 +12,9 @@ sys.path.append(os.getcwd())
 import carcade_settings
 
 
+PAGES_DIR = 'pages/'
+
+
 def path_for(name, language=None):
     url = ''
     if language != carcade_settings.DEFAULT_LANGUAGE:
@@ -41,16 +44,6 @@ def create_jinja2_environment(language):
     return env
 
 
-def render_template(template, context, target_fname):
-    target_dir = os.path.dirname(target_fname)
-    if not os.path.exists(target_dir):
-        os.makedirs(target_dir)
-
-    render = template.render(**context)
-    with codecs.open(target_fname, 'w', 'utf-8') as f:
-        f.write(render)
-
-
 def page_files(page_dir, language, md_or_yaml):
     """Yields files from `page_dir` which names matched to
     `*.<language>.<md|yaml>` pattern.
@@ -62,29 +55,8 @@ def page_files(page_dir, language, md_or_yaml):
             yield f
 
 
-def create_context(page_dir, language):
-    """Creates context by parsing all markdown- and yaml-files with given
-    `language` from `page_dir`.
-    """
-    context = {}
-
-    for f in page_files(page_dir, language, 'md'):
-        var_name, suffix = os.path.basename(f.name).split('.', 1)
-        context[var_name] = markdown.markdown(f.read())
-
-    for f in page_files(page_dir, language, 'yaml'):
-        data = yaml.load(f.read())
-        if data:
-            context.update(data)
-
-    return context
-
-
-class Page(object):
-    def __init__(self, name, target_filename, context):
-        self.name = name
-        self.context = context
-        self.target_filename = target_filename
+class Node(object):
+    def __init__(self):
         self.children = []
         self.ascendant = None
 
@@ -92,67 +64,88 @@ class Page(object):
         self.children.append(page)
         page.ascendant = self
 
+    def build(self, jinja2_env, build_directory):
+        for child in self.children:
+            child.build(jinja2_env, build_directory)
+
+
+class Page(Node):
+    def __init__(self, page_dir, language):
+        super(Page, self).__init__()
+        self.name = page_dir[len(PAGES_DIR):]
+        self.context = {'PAGE_NAME': self.name}
+
+        for f in page_files(page_dir, language, 'md'):
+            var_name, suffix = os.path.basename(f.name).split('.', 1)
+            self.context[var_name] = markdown.markdown(f.read())
+
+        for f in page_files(page_dir, language, 'yaml'):
+            data = yaml.load(f.read())
+            if data:
+                self.context.update(data)
+
+    def get_target_filename(self, build_dir, language):
+        return os.path.join(
+            build_dir, path_for(self.name, language=language), 'index.html')
+
+    def build(self, jinja2_env, build_directory):
+        super(Page, self).build(jinja2_env, build_directory)
+
+        # Retrieve template
+        template = jinja2_env.get_template(
+            carcade_settings.LAYOUTS.get(self.name, carcade_settings.DEFAULT_LAYOUT))
+
+        # Collect descendant page contexts
+        subpages = []
+        for subpage in self.children:
+            subpages.append(subpage.context)
+
+        # Collect sibling page contexts
+        index = None
+        siblings = []
+        for idx, sibling in enumerate(self.ascendant.children):
+            if sibling == self:  # TODO Introduce pagination
+                index = idx
+            siblings.append(sibling.context)
+
+        context = dict(self.context, **{
+            'SUBPAGES': subpages,  # TODO Make naming more consistent
+            'SIBLINGS': siblings,
+            'INDEX': index  # XXX
+        })
+        self.render_template(template, context, build_directory)
+
+    def render(self, template, context, build_directory):
+        target_filename = self.get_target_filename(
+            build_directory, template.globals['LANGUAGE'])
+        target_dir = os.path.dirname(target_filename)
+
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+
+        with codecs.open(target_filename, 'w', 'utf-8') as f:
+            f.write(template.render(**context))
+
 
 def build(build_dir):
-    PAGES_DIR = 'pages/'
-
     for language in carcade_settings.LANGUAGES:
         jinja2_env = create_jinja2_environment(language)
-        
+
         # Build site tree from down to top
         forest = {}  # `forest` contains growing site subtrees
         for root, directories, files in os.walk(PAGES_DIR, topdown=False):
-            page_name = root[len(PAGES_DIR):]
+            if root == PAGES_DIR:
+                break
 
-            context = create_context(root, language)
-            context['PAGE_NAME'] = page_name
-
-            target_filename = os.path.join(
-                build_dir, path_for(page_name, language=language), 'index.html')
-
-            page = Page(page_name, target_filename, context)
+            page = Page(root, language)
             for directory in directories:
-                child_name = os.path.join(page_name, directory)
+                child_name = os.path.join(page.name, directory)
                 child = forest.pop(child_name)  # Pop subtree from `forest`
                 page.add_child(child)  # Attach it to ascendant page
 
             forest[page.name] = page  # Put resulting subtree to the `forest`
 
-        root = forest.pop('')
-        assert not forest
-
-        build_recursive(jinja2_env, root)
-
-
-def build_recursive(jinja2_env, page):
-    for child in page.children:
-        build_recursive(jinja2_env, child)
-
-    if not page.name:
-        # `page` is root, nothing to build
-        return
-
-    # Retrieve template
-    template = jinja2_env.get_template(
-        carcade_settings.LAYOUTS.get(page.name, carcade_settings.DEFAULT_LAYOUT))
-
-    # Collect descendant page contexts
-    subpages = []
-    for subpage in page.children:
-        subpages.append(subpage.context)
-
-    # Collect sibling page contexts
-    index = None
-    siblings = []
-    for idx, sibling in enumerate(page.ascendant.children):
-        if sibling == page:  # TODO Introduce pagination
-            index = idx
-        siblings.append(sibling.context)
-
-    # Important: don't change stored context by adding new keys
-    context = dict(page.context, **{
-        'SUBPAGES': subpages,  # TODO Make naming more consistent
-        'SIBLINGS': siblings,
-        'INDEX': index  # XXX
-    })
-    render_template(template, context, page.target_filename)
+        root = Node()
+        for page in forest.values():
+            root.add_child(page)
+        root.build(jinja2_env, build_dir)

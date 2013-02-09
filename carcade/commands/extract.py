07ac695365e8bf411f8fae83a77a951c330cc065
Anton Romanovich <anthony.romanovich@gmail.com>
2013-02-09 13:52:19 +0600

    Refactor CLI

--- carcade/commands/extract.py
+++
@@ -1,7 +0,0 @@
-from carcade.i18n import extract_translations
-from carcade.environments import create_jinja2_env
-
-
-def main():
-    jinja2_env = create_jinja2_env()
-    extract_translations(jinja2_env, 'translations/messages.pot')

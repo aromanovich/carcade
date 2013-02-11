10a7cbeda6d5f86a95ef0c48b4f72781be1618ba
Anton Romanovich <anthony.romanovich@gmail.com>
2013-02-11 18:14:28 +0600

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='carcade',
-    version='0.0.5',
+    version='0.0.6',
     description='Static site generator',
     url='https://github.com/aromanovich/carcade',
     author='Anton Romanovich',
